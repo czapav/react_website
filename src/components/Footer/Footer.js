5f00fd2fc8e1ba0500e891cd747d0e9d464e3aa0
Daniel Czapko <daniel.czapko@gmail.com>
2019-02-20 19:39:22 +0100

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import FooterStyle from "./FooterStyle";
+import Container from "../Container/Container";
+const Footer = () => (
+  <FooterStyle>
+    <Container>
+      <p>
+        Shop © 2019 <br />
+        Designed by Daniel Czapko
+      </p>
+    </Container>
+  </FooterStyle>
+);
+
+export default Footer;
